59a03390e75c8d193e16f0b732652814141ac7ea
CeciliaAravena <88345326+CeciliaAravena@users.noreply.github.com>
2021-12-01 16:05:08 -0300

    se cargaron los archivos correspondientes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Content from "./components/Content";
+import NavBar from "./components/NavBar";
+import "./styles/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Este es el App.js</h1>
+     <NavBar/>
+     <Content/>
+    </div>
+  );
+}
+
+export default App;
