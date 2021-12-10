ce0abda9d8683ab12dd265c632ceb317e2e41cd3
CeciliaAravena <88345326+CeciliaAravena@users.noreply.github.com>
2021-12-10 11:33:19 -0300

    desafio contador con boton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import Content from "./components/Content";
+import ItemCount from "./components/ItemCount";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 import "./styles/styles.css";
 
 function App() {
+  const inicial = 1
+  const max = 10
+
+
+
   return (
     <center>
      <NavBar/>
      <ItemListContainer greeting ="Hola soy ItemListContainer" />
      <Content/>
+     <div clasName= 'hooks-app'>
+       <ItemCount inicial={inicial} max={max} />
+     </div>
+
     </center>
   );
 }
---
+++ src/helpers/procesoPesado.js
@@ -0,0 +1,6 @@
+export const procesoPesado = (interacciones) => {
+    for(let i= 0;i <interacciones; i ++){
+        console.log('en proceso...')
+    }
+    return `${ interecciones } interacciones reaizadas`
+}
